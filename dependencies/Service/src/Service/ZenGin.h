d67cd2e5b6604889d730a618c13624a74248a36a
TopLayer <vmi1401bsv04@gmail.com>
2026-02-19 22:22:01 +0500

    Remove Service folde

--- dependencies/Service/src/Service/ZenGin.h
+++
@@ -1,8 +0,0 @@
-#include <Service/ZenGin/Iterators.h>
-#include <Service/ZenGin/ActiveOption.h>
-#include <Service/ZenGin/Symbol.h>
-#include <Service/ZenGin/StringPool.h>
-#include <Service/ZenGin/ParserExtensions.h>
-#include <Service/ZenGin/StringFunctors.h>
-#include <Service/ZenGin/KeyCombo.h>
-#include <Service/ZenGin/Saveable.h>
